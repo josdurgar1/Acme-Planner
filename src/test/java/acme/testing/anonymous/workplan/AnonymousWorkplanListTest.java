c23684990ae69a01fd947f0711926c106bee664c
dancasnar1 <dancasnar1@alum.us.es>
2021-05-17 20:44:19 +0200

    List and show of Workplan by anonymous test finished

---
+++ src/test/java/acme/testing/anonymous/workplan/AnonymousWorkplanListTest.java
@@ -0,0 +1,36 @@
+package acme.testing.anonymous.workplan;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AnonymousWorkplanListTest extends AcmePlannerTest{
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/workplan/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void list(final int recordIndex, final String title, final String isPublic, final String isPublished, 
+		final String init, final String end, final String workload, final String executionPeriod) {
+		
+		super.clickOnMenu("Anonymous", "Workplan List");
+		
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.checkColumnHasValue(recordIndex, 1, isPublic);
+		super.checkColumnHasValue(recordIndex, 2, isPublished);
+		super.checkColumnHasValue(recordIndex, 3, init);
+		super.checkColumnHasValue(recordIndex, 4, end);
+		super.checkColumnHasValue(recordIndex, 5, workload);
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("isPublic", isPublic);
+		super.checkInputBoxHasValue("isPublished", isPublished);
+		super.checkInputBoxHasValue("executionPeriod", executionPeriod);
+		super.checkInputBoxHasValue("workload", workload);
+		super.checkInputBoxHasValue("init", init);	
+		super.checkInputBoxHasValue("end", end);
+	}
+}
