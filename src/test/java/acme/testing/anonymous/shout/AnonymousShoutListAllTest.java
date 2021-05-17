8b465a2aec9920d1c6837f9d19e486559ca4c960
dancasnar1 <dancasnar1@alum.us.es>
2021-05-17 10:42:10 +0200

    Shout tests done with create positive test with error

--- src/test/java/acme/testing/anonymous/shout/AnonymousShoutListAllTest.java
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutListAllTest.java
@@ -15,13 +15,13 @@ public class AnonymousShoutListAllTest extends AcmePlannerTest{
 	@ParameterizedTest
 	@CsvFileSource(resources = "/anonymous/shout/list-all.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void listAll(final int recordIndex, final String author, final String text, final String moment) {		
+	public void listAll(final int recordIndex, final String moment, final String author, final String text) {		
 
 		super.clickOnMenu("Anonymous", "Shout List");
 		
-		super.checkColumnHasValue(recordIndex, 0, author);
-		super.checkColumnHasValue(recordIndex, 1, text);
-		super.checkColumnHasValue(recordIndex, 2, moment);
+		super.checkColumnHasValue(recordIndex, 0, moment);
+		super.checkColumnHasValue(recordIndex, 1, author);
+		super.checkColumnHasValue(recordIndex, 2, text);
 		
 		
 
