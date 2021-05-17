1bb7b25f6c43a546f1a08f4a1ba215f3329c23b7
Luis <luispozas_sevilla@hotmail.com>
2021-05-17 11:48:52 +0200

    Test anonymous task done

---
+++ src/test/java/acme/testing/anonymous/task/AnonymousTaskListTest.java
@@ -0,0 +1,36 @@
+package acme.testing.anonymous.task;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AnonymousTaskListTest extends AcmePlannerTest{
+
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/task/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void list(final int recordIndex, final String title, final String iniMoment, final String endMoment, final String workload, final String exePeriod, final String description, final String link, final String visibility) {		
+		super.clickOnMenu("Anonymous", "Task List");
+		
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.checkColumnHasValue(recordIndex, 1, iniMoment);
+		super.checkColumnHasValue(recordIndex, 2, endMoment);
+		super.checkColumnHasValue(recordIndex, 3, description);
+		
+		super.clickOnListingRecord(recordIndex);
+		
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("initialMoment", iniMoment);
+		super.checkInputBoxHasValue("endMoment", endMoment);
+		super.checkInputBoxHasValue("workload", workload);
+		super.checkInputBoxHasValue("executionPeriod", exePeriod);	
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("link", link);
+		super.checkInputBoxHasValue("visibility", visibility);
+			
+
+	}
+}
