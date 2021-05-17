eaec6e01a47d8f7a00c2fb5aec1de308eeae16f8
dancasnar1 <dancasnar1@alum.us.es>
2021-05-17 11:01:33 +0200

    Admin Tests done

--- src/test/java/acme/testing/administrator/task/AdministratorTaskTest.java
+++ src/test/java/acme/testing/administrator/task/AdministratorTaskTest.java
@@ -12,7 +12,7 @@ public class AdministratorTaskTest extends AcmePlannerTest{
 	@CsvFileSource(resources = "/administrator/task/list.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void list(final int recordIndex, final String title, final String initialMoment, final String endMoment,
-		final String workload, final String description, final String link, final String executionPeriod, final String visibility) {	
+		final String workload, final String executionPeriod, final String description, final String link, final String visibility) {	
 		
 		super.signIn("administrator", "administrator");
 
@@ -21,7 +21,7 @@ public class AdministratorTaskTest extends AcmePlannerTest{
 		super.checkColumnHasValue(recordIndex, 0, title);
 		super.checkColumnHasValue(recordIndex, 1, initialMoment);
 		super.checkColumnHasValue(recordIndex, 2, endMoment);
-		super.checkColumnHasValue(recordIndex, 4, description);
+		super.checkColumnHasValue(recordIndex, 3, description);
 		
 		super.clickOnListingRecord(recordIndex);
 		
@@ -29,10 +29,10 @@ public class AdministratorTaskTest extends AcmePlannerTest{
 		super.checkInputBoxHasValue("initialMoment", initialMoment);
 		super.checkInputBoxHasValue("endMoment", endMoment);
 		super.checkInputBoxHasValue("workload", workload);
-		super.checkInputBoxHasValue("description", description);
-		super.checkInputBoxHasValue("link", link);
 		super.checkInputBoxHasValue("executionPeriod", executionPeriod);
+		super.checkInputBoxHasValue("description", description);
 		super.checkInputBoxHasValue("visibility", visibility);
+		super.checkInputBoxHasValue("link", link);
 		
 		super.signOut();
 	}
