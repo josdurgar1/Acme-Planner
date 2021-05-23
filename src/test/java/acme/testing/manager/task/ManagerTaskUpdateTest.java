faadb61a595adbadc5fd65f8ec9b622e5b50eb82
Luis <luispozas_sevilla@hotmail.com>
2021-05-23 11:34:03 +0200

    Test para el update task done

--- src/test/java/acme/testing/manager/task/ManagerTaskUpdateTest.java
+++ src/test/java/acme/testing/manager/task/ManagerTaskUpdateTest.java
@@ -10,14 +10,12 @@ public class ManagerTaskUpdateTest extends AcmePlannerTest{
 	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/manager/task/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(20)	
+	@Order(10)	
 	public void updatePositive(final int recordIndex, final String title, final String iniMoment, final String endMoment, final String exePeriod, final String workload, final String description, final String visibility, final String link) {		
 		super.signIn("manager", "manager");
 		
 		super.clickOnMenu("Manager", "My tasks");		
-		
-		super.checkColumnHasValue(recordIndex, 0, title);
-		
+				
 		super.clickOnListingRecord(recordIndex);
 		
 		super.fillInputBoxIn("title", title);
@@ -37,16 +35,31 @@ public class ManagerTaskUpdateTest extends AcmePlannerTest{
 		super.checkColumnHasValue(recordIndex, 5, description);
 		super.checkColumnHasValue(recordIndex, 6, visibility);
 		
+		
+		super.signOut();
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)	
+	public void updateNegative(final int recordIndex, final String title, final String iniMoment, final String endMoment, final String exePeriod, final String workload, final String description, final String visibility, final String link) {		
+		super.signIn("manager", "manager");
+		
+		super.clickOnMenu("Manager", "My tasks");		
+		
+		
 		super.clickOnListingRecord(recordIndex);
 		
-		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("initialMoment", iniMoment);
-		super.checkInputBoxHasValue("endMoment", endMoment);
-		super.checkInputBoxHasValue("workload", workload);
-		super.checkInputBoxHasValue("description", description);
-		super.checkInputBoxHasValue("executionPeriod", exePeriod);	
-		super.checkInputBoxHasValue("link", link);
-		super.checkInputBoxHasValue("visibility", visibility);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("initialMoment", iniMoment);
+		super.fillInputBoxIn("endMoment", endMoment);
+		super.fillInputBoxIn("workload", workload);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("link", link);
+		super.fillInputBoxIn("visibility", visibility);
+		super.clickOnSubmitButton("Update");
+		super.checkErrorsExist();
+
 		
 		super.signOut();
 	}
