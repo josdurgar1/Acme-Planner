25eba91475c3ce2d24d0512d54a2f25e7748a10e
dancasnar1 <dancasnar1@alum.us.es>
2021-05-24 16:55:54 +0200

    Comentarios explicando los tests en: AdministratorDashboardShowTest AnonymousShoutCreateTest

--- src/test/java/acme/testing/administrator/dashboard/AdministratorDashboardShowTest.java
+++ src/test/java/acme/testing/administrator/dashboard/AdministratorDashboardShowTest.java
@@ -8,10 +8,11 @@ public class AdministratorDashboardShowTest extends AcmePlannerTest{
 
 	@Test
 	public void seeDashboard() {
+		//Nos logeamos como administrador
 		super.signIn("administrator", "administrator");
-		
+		//Pulsamos sobre el menú de administrador y clickamos en la subopción Dashboard para ver la dashboard
 		super.clickOnMenu("Administrator", "Dashboard");
-		
+		//Cerramos sesión
 		super.signOut();
 	}
 }
--- src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
@@ -15,17 +15,17 @@ public class AnonymousShoutCreateTest extends AcmePlannerTest{
 		@CsvFileSource(resources = "/anonymous/shout/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 		@Order(10)
 		public void createPositive(final int recordIndex, final String author, final String text, final String info) {
-
+			//Clickamos en el menú de anonymous y seleccionamos la subopción Shout donde nos aparecerá el formulario
 			super.clickOnMenu("Anonymous", "Shout");
-
+			//Rellenamos los campos con los parámetros que pasamos mediante el fichero .csv
 			super.fillInputBoxIn("author", author);
 			super.fillInputBoxIn("text", text);
 			super.fillInputBoxIn("info", info);
-			
+			//Clickamos en el botón Shout! el cual creará el Shout con los datos anteriores
 			super.clickOnSubmitButton("Shout!");
-
+			//Clickamos en el menú de anonymous y seleccionamos la subopción Shout List donde nos aparecerá el listado de Shouts
 			super.clickOnMenu("Anonymous", "Shout List");
-			
+			//Comprobamos que el Shout creado esté en la lista con los campos correspondientes
 			super.checkColumnHasValue(recordIndex, 1, author);
 			super.checkColumnHasValue(recordIndex, 2, text);
 
@@ -35,13 +35,15 @@ public class AnonymousShoutCreateTest extends AcmePlannerTest{
 		@CsvFileSource(resources = "/anonymous/shout/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 		@Order(20)
 		public void createNegative(final int recordIndex, final String author, final String text, final String info) {
+			//Clickamos en el menú de anonymous y seleccionamos la subopción Shout donde nos aparecerá el formulario
 			super.clickOnMenu("Anonymous", "Shout");
-
+			//Rellenamos los campos con los parámetros que pasamos mediante el fichero .csv
 			super.fillInputBoxIn("author", author);
 			super.fillInputBoxIn("text", text);
 			super.fillInputBoxIn("info", info);
+			//Clickamos en el botón Shout! el cual creará el Shout con los datos anteriores
 			super.clickOnSubmitButton("Shout!");
-
+			//Checkeamos los distintos errores que nos dará al crear los shouts de formas negativas
 			super.checkErrorsExist();
 		}
 
